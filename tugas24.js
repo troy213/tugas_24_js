718a5c1f64cb18dc67de815df335c1f11257a3bb
Tritera_Erlangga-Niomic <tritera.erlangga@gmail.com>
2020-05-14 20:52:05 +0700

    Tugas 24 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas24.js
@@ -0,0 +1,5 @@
+var arr1 = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+console.log("Sebelumnya :",arr1.toString());
+console.log("Ascending :",arr1.sort().toString());
+console.log("Descending :",arr1.sort().reverse().toString());
